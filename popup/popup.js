3fcda9db9256f5a030a49898218abb5a9bb1098a
thedmdim <dmitruk.dd@tk23.ru>
2025-02-02 22:45:24 +0400

    refactor

--- popup/popup.js
+++ popup/popup.js
@@ -4,15 +4,11 @@ import searchEngines from "../shared/search-engines.js";
 document.addEventListener("DOMContentLoaded", async () => {
     let [fieldset] = document.getElementsByTagName("fieldset");
 
-    let { selectedSearchEngineName } = await browser.storage.local.get("selectedSearchEngineName")
-    searchEngines.forEach((se) => {
-        se.parsedURL = new URL(se.url)
-        let params = Array.from(se.parsedURL.searchParams.keys())
-        if (params.length == 1) {
-            se.q = params[0]
-        } else {
-            console.error("more than 1 q param in searchEngines item")
-        }
+    const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
+    const url = new URL(tab.url);
+    const currSe = searchEngines.find(se => se.url.hostname == url.hostname && (url.pathname == "/" || se.url.pathname == url.pathname))
+
+    searchEngines.forEach(se => {
 
         const label = document.createElement("label");
         label.style.display = "block";
@@ -23,11 +19,9 @@ document.addEventListener("DOMContentLoaded", async () => {
         input.name = "search-engine"
         input.value = se.name
 
-        if (input.value == selectedSearchEngineName) {
+        if (currSe && input.value == currSe.name) {
             input.setAttribute('checked', 'checked');
         }
-        
-
         
         label.appendChild(input)
         label.innerHTML += se.name
@@ -36,25 +30,19 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     fieldset.addEventListener("change", async (event) => {
         if (event.target.name === "search-engine") {
-            const [currTab] = await browser.tabs.query({ active: true, currentWindow: true });
-            const currURL = new URL(currTab.url);
-            const currSearch = searchEngines.find(se => se.parsedURL.hostname == currURL.hostname && (currURL.pathname == "/" || se.parsedURL.pathname == currURL.pathname))
-            
-            let newSearch = searchEngines.find(se => se.name == event.target.value)
+            let newSe = searchEngines.find(se => se.name == event.target.value)
 
-            if (typeof currSearch === "undefined") {
-                browser.tabs.create( { url: newSearch.parsedURL.protocol + newSearch.parsedURL.hostname } )
-            } else {
-                let currSeachQuery = currURL.searchParams.get(currSearch.q)
-                if (currSeachQuery) {
-                    newSearch.parsedURL.searchParams.set(newSearch.q, currSeachQuery)
-                    browser.tabs.update(currTab.id, { url: newSearch.parsedURL.href });
+            if (currSe) {
+                let currq = url.searchParams.get(currSe.q)
+                if (currq) {
+                    newSe.url.searchParams.set(newSe.q, currq)
+                    browser.tabs.update(tab.id, { url: newSe.url.href });
                 } else {
-                    browser.tabs.update(currTab.id, { url: newSearch.parsedURL.protocol + newSearch.parsedURL.hostname });  
+                    browser.tabs.update(tab.id, { url: newSe.url.protocol + "//" + newSe.url.hostname });  
                 }
+            } else {
+                browser.tabs.create( { url: newSe.url.protocol + "//" + newSe.url.hostname } )
             }
-
-            browser.storage.local.set({"selectedSearchEngineName": event.target.value})
         }
     });
 });
--- shared/search-engines.js
+++ shared/search-engines.js
@@ -4,6 +4,14 @@ const searchEngines = [
   { name: "Yandex", url: "https://ya.ru/search/?text=" },
 ];
 
-
+searchEngines.forEach(se => {
+  se.url = new URL(se.url)
+  let params = Array.from(se.url.searchParams.keys())
+  if (params.length == 1) {
+      se.q = params[0]
+  } else {
+      console.error("more than 1 q param in searchEngines item")
+  }
+})
 
 export default searchEngines;
