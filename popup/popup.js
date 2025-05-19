764b19857af6a92cc8533f2c45eb2a5c6b06f239
thedmdim <thedmdim@gmail.com>
2025-05-19 23:57:52 +0400

    dark theme

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", async () => {
+
+    // theme
+    let { theme } = await browser.storage.local.get("theme");
+    if (!theme || theme === "auto") {
+        theme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    }
+    document.documentElement.setAttribute("theme", theme);
     
+    // search engines list
     let { TextSearchEngines } = await browser.storage.local.get("TextSearchEngines")
 
     let [fieldset] = document.getElementsByTagName("fieldset");
@@ -7,12 +15,12 @@ document.addEventListener("DOMContentLoaded", async () => {
     const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
     const tabUrl = new URL(tab.url);
 
-    let currSearch
+    let currSearchEngine
     for (let i in TextSearchEngines) {
         let search = TextSearchEngines[i]
         let url = new URL(search.url)
         if (url.hostname == tabUrl.hostname) {
-            currSearch = search
+            currSearchEngine = search
             break
         }
     }
@@ -33,7 +41,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         input.name = "search-engine"
         input.value = search.name
 
-        if (currSearch && currSearch.name == input.value) {
+        if (currSearchEngine && currSearchEngine.name == input.value) {
             input.setAttribute('checked', 'checked');
         }
         
@@ -44,26 +52,27 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     fieldset.addEventListener("change", async (event) => {
         if (event.target.name === "search-engine") {
-            let newSe = TextSearchEngines.find(e => e.name == event.target.value)
-            let url = new URL(newSe.url)
+            let nextSearchEngine = TextSearchEngines.find(e => e.name == event.target.value)
+            let nextURL = new URL(nextSearchEngine.url)
 
-            if (currSearch) {
+            if (currSearchEngine) {
+
                 let { lastq } = await browser.storage.local.get("lastq")
-                let currq = tabUrl.searchParams.get(currSearch.qparam)
-                let q = currq || lastq  
-                if (!q && tabUrl.pathname == "/") {
-                    browser.tabs.update(tab.id, { url: url.protocol + "//" + url.hostname });  
+                let currq = tabUrl.searchParams.get(currSearchEngine.qparam)
+
+                if (tabUrl.pathname == "/" && !currq && !currSearchEngine.useLastq) {
+                    browser.tabs.update(tab.id, { url: nextURL.origin });
                     return
                 }
-                                
-                if (q) {
-                    url.searchParams.set(newSe.qparam, q)
-                    browser.tabs.update(tab.id, { url: url.href });
-                    browser.storage.local.set({ lastq: q })
-                    return
-                }
+
+                let q = currq || lastq
+                nextURL.searchParams.set(nextSearchEngine.qparam, q)
+                browser.tabs.update(tab.id, { url: nextURL.href });
+                browser.storage.local.set({ lastq: q })
+                return
+                       
             } else {
-                browser.tabs.create( { url: url.protocol + "//" + url.hostname } )
+                browser.tabs.create( { url: nextURL.origin } )
             }
         }
     });
